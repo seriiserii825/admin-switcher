5bf69e89a3fe0443a062150d1ddeb4952eeae143
serii <seriiburduja@gmail.com>
2023-12-20 18:05:28 +0200

    feat: extension works

--- dist/js/content.js
+++ dist/js/content.js
@@ -1,90 +1,76 @@
 let lastClipboardData = "";
 let noCheckClipboard = false;
+const origin = window.location.origin;
+const wp_admin = origin + '/wp-admin';
+const gestione = origin + '/gestione';
 
-const writeClipboard = async (text) => {
-  noCheckClipboard = true;
-  lastClipboardData = text;
 
-  if (navigator.clipboard && window.isSecureContext) {
-    await navigator.clipboard.writeText(text);
-    if (Toastify) {
-      Toastify({
-        text: "Text copied to clipboard",
-        duration: 3000,
-        gravity: "top",
-        position: "right",
-      }).showToast();
+function goToFront() {
+    function isUrlValid(url) {
+        return fetch(url, {method: 'HEAD'})
+            .then(response => {
+                return response.ok;
+            })
+            .catch(() => {
+                return false;
+            });
     }
-    noCheckClipboard = false;
-  } else {
-    // Use the 'out of viewport hidden text area' trick
-    const textArea = document.createElement("textarea");
-    textArea.value = text;
 
-    // Move textarea out of the viewport so it's not visible
-    textArea.style.position = "absolute";
-    textArea.style.left = "-999999px";
+    isUrlValid(wp_admin)
+        .then(valid => {
+            if (valid) {
+                window.location = wp_admin;
+            } else {
+                isUrlValid(gestione)
+                    .then(valid => {
+                        if (valid) {
+                            window.location = gestione;
+                        } else {
+                            console.log('url non valid')
+                        }
+                    })
+                    .catch(error => {
+                        console.error('An error occurred while checking the URL:', error);
+                    });
+            }
+        })
+        .catch(error => {
+            console.error('An error occurred while checking the URL:', error);
+        });
+}
 
-    document.body.prepend(textArea);
-    textArea.select();
-
-    try {
-      document.execCommand("copy");
-      if (Toastify) {
-        Toastify({
-          text: "Text copied to clipboard",
-          duration: 3000,
-          gravity: "top",
-          position: "right",
-        }).showToast();
-        noCheckClipboard = false;
-      }
-    } catch (error) {
-      console.error(error);
-      noCheckClipboard = false;
-    } finally {
-      textArea.remove();
-      noCheckClipboard = false;
-    }
-  }
-};
-
-function clipboardEnable() {
-  let selecter = window.getSelection().toString();
-  console.log(selecter, "selecter");
-  console.log(selecter.length, "selecter.length");
-  const is_true = selecter.length > 0;
-  console.log(is_true, "is_true");
-  if (is_true) {
-    console.log(selecter, "selecter");
-    writeClipboard(selecter);
-  }
+function goToAdmin() {
+    window.location = origin;
 }
 
 const startHandleClipboard = () => {
-  document.addEventListener("mouseup", clipboardEnable);
+    document.addEventListener("mouseup", goToFront);
 };
 const stopHandleClipboard = () => {
-  document.removeEventListener("mouseup", clipboardEnable);
+    document.removeEventListener("mouseup", goToAdmin);
 };
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  switch (request.type) {
-    case "enable": {
-      if (request.value) {
-        startHandleClipboard();
-      } else {
-        stopHandleClipboard();
-      }
-      sendResponse({ ok: true });
-      return;
+    switch (request.type) {
+        case "enable": {
+            if (request.value) {
+                console.log('value true')
+                goToFront();
+                // startHandleClipboard();
+            } else {
+                console.log('value false')
+                goToAdmin();
+                // stopHandleClipboard();
+            }
+            sendResponse({ok: true});
+            return;
+        }
     }
-  }
-  sendResponse({ ok: true });
+    sendResponse({ok: true});
 });
-chrome.runtime.sendMessage({ type: "status" }, (res) => {
-  if (res?.active) {
-    startHandleClipboard();
-  } else {
-    stopHandleClipboard();
-  }
+chrome.runtime.sendMessage({type: "status"}, (res) => {
+    if (res?.active) {
+        startHandleClipboard();
+    } else {
+        stopHandleClipboard();
+    }
 });
